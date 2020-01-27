50e0c4b62f473028c94cce56a25a9923f4239f92
Aldair <aldair.alda27@gmail.com>
2020-01-27 11:09:34 -0600

    Adding Gs with new sensor

--- GroundStation/sensorPerformance.py
+++ GroundStation/sensorPerformance.py
@@ -3,16 +3,94 @@
 #Only take values from serial ports, calculate height, pressure and temp. 
 
 import serial
+import matplotlib.pyplot as plt 
+from drawnow import *
 
 
-ser = serial.Serial('/dev/ttyACM0', 19200, timeout=1)  #Linux
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  #Linux
 #ser = serial.Serial('COM4', 19200, timeout=1) #Windows
 
-dataFrameSize = 0  #Define number of elements to receive by Serial Port, this help to discard invalid/wrong values received, and to split values for his processing.
+dataFrameSize = 3  #Define number of elements to receive by Serial Port, this help to discard invalid/wrong values received, and to split values for his processing.
+dataArrayClean = []
+
+temps_array = []
+heights_array = []
+press_array = []
+
+
+plt.ion() #To make interacive mode in plotting
+fig = plt.figure()
+
+
 
 def readData():
-    dataFrame = ser.readline()   #String/frame of data readed.
-    dataArray = dataFrame.split(",")      #Splitting data in to an array by the "," between each value
 
-    if len(dataArray) == dataFrameSize:
-        print (dataArray)
+    dataFrame = ser.readline()   #String/frame of data readed.
+    dataArray = dataFrame.split(",")   #Splitting data in to an array by the "," between each value
+    #dataArrayClean.append(dataArray[0].replace)
+    nOfDecPoints = []
+    filterDigits = []
+    aux = dataArray
+
+    
+
+    if len(dataArray) == dataFrameSize+1:
+        for k in range (0,dataFrameSize):
+            nOfDecPoints.append(aux[k].count('.'))
+            aux[k].replace('.','')
+            aux[k].replace('-','')
+            filterDigits.append(aux[k].isdigit())
+
+            
+        #print (dataArray)
+        temp = float(dataArray[0])
+        height = float(dataArray[1])
+        press = float(dataArray[2])
+        
+        temps_array.append(temp)
+        heights_array.append(height)
+        press_array.append(press)
+
+        #print (temp,height,press_array)
+
+        if len(heights_array) >100:
+            heights_array.pop(0)
+            temps_array.pop(0)
+            press_array.pop(0)
+        
+    else :
+        print("Not data received")
+
+def makeFig():
+    ax1 = fig.add_subplot(221)
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('F')
+    ax1.set_title('Temperatures')
+
+    ax2 = fig.add_subplot(222)
+    ax2.set_xlabel('time')
+    ax2.set_ylabel('Pa')
+    ax2.set_title('Pressures')
+
+    ax3 = fig.add_subplot(212)
+    ax3.set_xlabel('time')
+    ax3.set_ylabel('m')
+    ax3.set_title('Heights')
+
+
+
+    ax1.plot(temps_array,'ro-',label="Farenheit")
+    ax1.legend(loc="upper left")
+
+    ax2.plot(press_array,'g^-',label="Pascals")
+    ax2.legend(loc="upper right")
+
+    ax3.plot(heights_array,'b.-',label="Meters over sea level")
+    ax3.legend(loc='upper left')
+
+def main():
+    while 1:
+        readData()
+        drawnow(makeFig)
+
+if __name__ == '__main__': main()
--- Satellite/Main_Cansat/Main_Cansat.ino
+++ Satellite/Main_Cansat/Main_Cansat.ino
@@ -1,25 +1,11 @@
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMP280 Breakout
-  ----> http://www.adafruit.com/products/2651
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+//#include "../libraries/L3G4200D/L3G4200D.h"
+//#include "../libraries/ADXL345/ADXL345.h"
 
-#define BMP_SCK  (13)
+/* SPI port definition for BMP280 */
+#define BMP_SCK  (13) 
 #define BMP_MISO (12)
 #define BMP_MOSI (11)
 #define BMP_CS   (10)
@@ -33,7 +19,7 @@ void setup() {
   //Serial.println(F("BMP280 test"));
 
   if (!bmp.begin()) {
-    //Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
 
@@ -41,13 +27,14 @@ void setup() {
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X2,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16);     /* Filtering. */
+                  Adafruit_BMP280::FILTER_X8);     /* Filtering. */
                   //Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
 }
 
 void loop() {
     //Serial.print(F("Temperature = "));
-    //Serial.print(bmp.readTemperature());
+    Serial.print(bmp.readTemperature());
+    Serial.print(",");
     //Serial.println(" *C");
 
     //Serial.print(F("Pressure = "));
@@ -57,9 +44,11 @@ void loop() {
     //Serial.print(F("Approx altitude = "));
     Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
     Serial.print(",");
-    Serial.println();
     //Serial.print(',');
     //Serial.println(" m");
     //Serial.println();
-    delay(30);
+    Serial.print(bmp.readPressure());
+    Serial.print(",");
+    Serial.println();
+    delay(100);
 }
